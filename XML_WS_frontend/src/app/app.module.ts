0530e4761226e70a4ea2d236cbb7e870dc403686
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2022-12-01 01:04:39 +0100

    [#2] Registracija - front - Ubaceni moduli za anuglar materijale - Materijali se importuju preko materials.module.ts
    - Ubacen bootstrap
    
    - Napravljena komponenta registration
    - Implementiran izgled stranice za registraciju

--- XML_WS_frontend/src/app/app.module.ts
+++ XML_WS_frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialsModule } from './materials/materials.module';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
