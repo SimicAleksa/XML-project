0df968290d23770cbc991e650006fb93a86207e5
unknown <nevenaradesic@gmail.com>
2022-12-13 15:34:48 +0100

    dodate klase za autorska prava

--- XML_WS_backend/src/main/java/com/example/XML_WS_backend/jaxb/JaxBParser.java
+++ XML_WS_backend/src/main/java/com/example/XML_WS_backend/jaxb/JaxBParser.java
@@ -2,11 +2,14 @@ package com.example.XML_WS_backend.jaxb;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.util.HashMap;
 
+import com.example.XML_WS_backend.models.zahtev_za_autorska_prava.ZahtevZaAutorskaPrava;
 import com.example.XML_WS_backend.models.zahtev_za_patent.ZahtevZaPatent;
+import com.sun.xml.internal.ws.db.glassfish.BridgeWrapper;
 
 public class JaxBParser {
     private HashMap<Class, String> schemaLocationRegistry = new HashMap<>();
@@ -17,29 +20,23 @@ public class JaxBParser {
 
     public void test() {
         try {
-//            System.out.println("Working Directory = " + System.getProperty("user.dir"));
-            System.out.println("[INFO] Testiranje: JAXB XML Schema validation.\n");
-
-            // Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
-            JAXBContext context = JAXBContext.newInstance("com.example.XML_WS_backend.models.zahtev_za_patent");
-
-            // Unmarshaller - zadužen za konverziju iz XML-a u objektni model
+//
+            JAXBContext context = JAXBContext.newInstance("com.example.XML_WS_backend.models.zahtev_za_autorska_prava");
             Unmarshaller unmarshaller = context.createUnmarshaller();
-
-            // Učitavanje XML-a u objektni model
-            ZahtevZaPatent zahtevZaPatent = (ZahtevZaPatent) unmarshaller.unmarshal(new File("data\\patent\\ZAHTEV_ZA_PRIZNANJE_PATENTA.xml"));
-
-            // Ispis sadržaja objekta
+            ZahtevZaAutorskaPrava zahtevZaPrava = (ZahtevZaAutorskaPrava) unmarshaller.unmarshal(new File("data\\autorska_prava\\ZAHTEV_ZA_AUTORSKA_PRAVA.xml"));
             System.out.println("[INFO] Unmarshalled content:");
-            System.out.println(zahtevZaPatent);
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+            marshaller.marshal(zahtevZaPrava, System.out);
+            System.out.println("---------------------------------------------------------------------------");
+            zahtevZaPrava.setPseudonimIliZnakAutora("Novi pseudonim autora");
+            File xmlFile = new File("data\\autorska_prava\\Izmenjeno.xml");
+            marshaller.marshal(zahtevZaPrava, xmlFile);
+            marshaller.marshal(zahtevZaPrava, System.out);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-//    public static void main( String[] args ) {
-//        JaxBParser test = new JaxBParser();
-//        test.test();
-//    }
 }
---
+++ XML_WS_backend/src/main/java/com/example/XML_WS_backend/models/zahtev_za_autorska_prava/ObjectFactory.java
@@ -0,0 +1,128 @@
+
+package com.example.XML_WS_backend.models.zahtev_za_autorska_prava;
+
+import javax.xml.bind.annotation.XmlRegistry;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the rs.ac.uns.ftn.autorskapravalatinica package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: rs.ac.uns.ftn.autorskapravalatinica
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava }
+     * 
+     */
+    public ZahtevZaAutorskaPrava createZahtevZaAutorskaPrava() {
+        return new ZahtevZaAutorskaPrava();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciODelu }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciODelu createZahtevZaAutorskaPravaPodaciODelu() {
+        return new ZahtevZaAutorskaPrava.PodaciODelu();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciOPodnosiocu }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciOPodnosiocu createZahtevZaAutorskaPravaPodaciOPodnosiocu() {
+        return new ZahtevZaAutorskaPrava.PodaciOPodnosiocu();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice createZahtevZaAutorskaPravaPodaciOPodnosiocuLice() {
+        return new ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciOAutoru }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciOAutoru createZahtevZaAutorskaPravaPodaciOAutoru() {
+        return new ZahtevZaAutorskaPrava.PodaciOAutoru();
+    }
+
+    /**
+     * Create an instance of {@link TPunomocnik }
+     * 
+     */
+    public TPunomocnik createTPunomocnik() {
+        return new TPunomocnik();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.Prilozi }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.Prilozi createZahtevZaAutorskaPravaPrilozi() {
+        return new ZahtevZaAutorskaPrava.Prilozi();
+    }
+
+    /**
+     * Create an instance of {@link TAdresa }
+     * 
+     */
+    public TAdresa createTAdresa() {
+        return new TAdresa();
+    }
+
+    /**
+     * Create an instance of {@link TPodnosilac }
+     * 
+     */
+    public TPodnosilac createTPodnosilac() {
+        return new TPodnosilac();
+    }
+
+    /**
+     * Create an instance of {@link TAutor }
+     * 
+     */
+    public TAutor createTAutor() {
+        return new TAutor();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciODelu.PodaciODeluPrerade }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciODelu.PodaciODeluPrerade createZahtevZaAutorskaPravaPodaciODeluPodaciODeluPrerade() {
+        return new ZahtevZaAutorskaPrava.PodaciODelu.PodaciODeluPrerade();
+    }
+
+    /**
+     * Create an instance of {@link ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice.PravnoLice }
+     * 
+     */
+    public ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice.PravnoLice createZahtevZaAutorskaPravaPodaciOPodnosiocuLicePravnoLice() {
+        return new ZahtevZaAutorskaPrava.PodaciOPodnosiocu.Lice.PravnoLice();
+    }
+
+}
