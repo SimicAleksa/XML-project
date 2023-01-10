8a06fd2a595c85e6b51b17fa89ce2954ed309fc3
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2023-01-10 16:49:43 +0100

    Inicijalizacija zasebnog backend projekta za Zig

---
+++ XML_WS_Trademark_backend/src/main/java/com/example/XML_WS_Trademark_backend/XmlWsTrademarkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.XML_WS_Trademark_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlWsTrademarkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlWsTrademarkBackendApplication.class, args);
+	}
+
+}
