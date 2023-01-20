824593329b7fd17779c05138ed231d47c3335b40
EvilPopS <strahinjapopovic.evilpops@gmail.com>
2023-01-20 02:10:29 +0100

    Inicijalizacija zasebnih backend-a za patent i autorksa prava

---
+++ XML_WS_AuthorRights_backend/src/main/java/com/example/XML_WS_AuthorRights_backend/XmlWsAuthorRightsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.XML_WS_AuthorRights_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlWsAuthorRightsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlWsAuthorRightsBackendApplication.class, args);
+	}
+
+}
---
+++ XML_WS_Patent_backend/src/main/java/com/example/XML_WS_Patent_backend/XmlWsPatentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.XML_WS_Patent_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlWsPatentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlWsPatentBackendApplication.class, args);
+	}
+
+}
