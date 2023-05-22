6e6d45b754f0722fccddf09b21961948e4765e81
SimicAleksa <aleksadsimic@gmail.com>
2023-05-22 15:48:06 +0200

    front patent dobar napredak

--- XML_WS_frontend/src/app/components/patent/new-patent-form/new-patent-form.component.ts
+++ XML_WS_frontend/src/app/components/patent/new-patent-form/new-patent-form.component.ts
@@ -10,7 +10,6 @@ import { XMLParser } from 'src/app/utils/XMLParser';
   styleUrls: ['./new-patent-form.component.css']
 })
 export class NewPatentFormComponent implements OnInit {
-  public podnosilac: { name: string, selected: boolean, representative: boolean, form: any };
   items: any[] = [];
   form: FormGroup;
 
@@ -44,8 +43,19 @@ export class NewPatentFormComponent implements OnInit {
   private numInpFieldValids = [Validators.required, Validators.pattern(this.numReg)];
 
 
+  public podnosilacJeIPronalazac:boolean = false;
+  public pronalazacNeZeliDaBudeNaveden:boolean = false;
+  public pronalazacJeIPodnosilac:boolean = false;
+  public zaZastupanje:boolean = false;
+  public zaPrijemPismena:boolean = false;
+  public dopunska:boolean = false;
+  public izdvojena:boolean = false;
+  public elektronskimPutem:boolean = false;
+  public papirnaForma:boolean = false;
+
   private podnosilacInfoFBObj = {
     ime: ['', this.nameInpFieldValids],
+    prezime: ['', this.nameInpFieldValids],
     telefon: ['', this.numInpFieldValids],
     eMail: ['', [Validators.required, Validators.email]],
     faks: ['', this.numInpFieldValids],
@@ -60,6 +70,7 @@ export class NewPatentFormComponent implements OnInit {
 
   private pronalazacInfoFBObj = {
     ime: ['', this.nameInpFieldValids],
+    prezime: ['', this.nameInpFieldValids],
     telefon: ['', this.numInpFieldValids],
     eMail: ['', [Validators.required, Validators.email]],
     faks: ['', this.numInpFieldValids],
@@ -68,18 +79,22 @@ export class NewPatentFormComponent implements OnInit {
     postanskiBroj: ['', this.numInpFieldValids],
     mestoPrebivalista: ['', this.nameInpFieldValids],
     drzavaPrebivalista: ['', this.nameInpFieldValids],
-    pronalazacNeZeliDaBudeNaveden:false
+    pronalazacNeZeliDaBudeNaveden:false,
+    pronalazacJeIPodnosilac:false
   };
 
 
   private punomocnikInfoFBObj = {
     ime: ['', this.nameInpFieldValids],
+    prezime: ['', this.nameInpFieldValids],
     telefon: ['', this.numInpFieldValids],
     eMail: ['', [Validators.required, Validators.email]],
     ulica: ['', this.nameInpFieldValids],
     brojUlice: ['', this.numInpFieldValids],
     postanskiBroj: ['', this.numInpFieldValids],
     mestoPrebivalista: ['', this.nameInpFieldValids],
+    drzavaPrebivalista: ['', this.nameInpFieldValids],
+    drzavljanstvo: ['', this.nameInpFieldValids],
     zaZastupanje:false,
     zaPrijemPismena:false
   };
@@ -94,6 +109,7 @@ export class NewPatentFormComponent implements OnInit {
     brojUliceDostavljanje: ['', this.numInpFieldValids],
     postanskiBrojDostavljanje: ['', this.numInpFieldValids],
     mestoPrebivalistaDostavljanje: ['', this.nameInpFieldValids],
+    drzavaPrebivalistaDostavljanje: ['', this.nameInpFieldValids],
     elektronskimPutem:false,
     papirnaForma:false
   };
@@ -114,8 +130,8 @@ export class NewPatentFormComponent implements OnInit {
   
 
   public podnosiocZahteva = this._formBuilder.group(this.podnosilacInfoFBObj);
-  public pronalazac = this._formBuilder.group(this.podnosilacInfoFBObj);
-  public punomocnik = this._formBuilder.group(this.podnosilacInfoFBObj);
+  public pronalazac = this._formBuilder.group(this.pronalazacInfoFBObj);
+  public punomocnik = this._formBuilder.group(this.punomocnikInfoFBObj);
   public podaciPatenta = this._formBuilder.group(this.pronalazakInfoFBObj);
   public podaciPosiljke = this._formBuilder.group(this.posiljkaInfoFBObj);
   public ostaloVrsta = this._formBuilder.group(this.vrstaPrijaveInfoFBObj);
@@ -124,126 +140,31 @@ export class NewPatentFormComponent implements OnInit {
   public numbers = Array.from({length: 45}, (_, i) => i+1);
 
   constructor(private _formBuilder: FormBuilder, private xmlParser: XMLParser, private requestMaker: RequestMaker, private lStoargeManager: LocalStorageManager) {
-    this.podnosilac = {name: "Podnosioc 1", selected: true, representative: false, form: this.getPodnosiocFromForm()};
-    this.podnosilac.representative = true;
-    // this.form = this._formBuilder.group({
-    //   ostaloRanije : this._formBuilder.array(this.ranijePrijaveInfoFBObj)
-    // })
-  }
-
-  // ostaloRanije: FormGroup;
-
-  private area(){
-    return this._formBuilder.group({
-      brojPrvobitnePrijave: [''],
-      datumPodnosenjaPrvobitnePrijave: [''],
-      dvoslovnaOznakaDrzave: [''],
-    });
-  }
-
-  private areaFULL(){
-    return this._formBuilder.group({
-      brojPrvobitnePrijave: ['asdasddas'],
-      datumPodnosenjaPrvobitnePrijave: ['dsadassa'],
-      dvoslovnaOznakaDrzave: ['dsadsadsa'],
-    });
   }
 
   ngOnInit(): void {
-  //   this.ostaloRanije = this._formBuilder.group({
-  //       areas:this._formBuilder.array([this.area()])
-  //   });    
-  // // this.addNewInput()
-  // console.log(this._formBuilder.array([this.area()]));
+
   }
 
-
-  // getControls() {
-  //   // console.log(this.ostaloRanije.get('areas') as FormArray);
-    
-  //   return (this.ostaloRanije.get('areas') as FormArray).controls;
-  // }
-
   addNewInput(): void {
-
-    // const control = <FormArray>this.ostaloRanije.get('areas');
-    // control.push(this.areaFULL());
-
     alert("DODATO")
     const newItem = {
       brojPrvobitnePrijave: this.ostaloRanije.get("brojPrvobitnePrijave")?.value,
       datumPodnosenjaPrvobitnePrijave: this.ostaloRanije.get("datumPodnosenjaPrvobitnePrijave")?.value,
       dvoslovnaOznakaDrzave: this.ostaloRanije.get("dvoslovnaOznakaDrzave")?.value,
     };
-
     this.items.push(newItem);
-
     this.ostaloRanije = this._formBuilder.group(this.ranijePrijaveInfoFBObj);
 
   }
 
-
-  getFormControlName(index: number): string {
-    return `items[${index}].brojPrvobitnePrijave`;
-  }
-
-  getPodnosiocFromForm() {
-    let p = this.podnosiocZahteva.value;
-    return {
-      ime: p.ime,
-      telefon: p.telefon,
-      eMail: p.eMail,
-      faks: p.faks,
-      ulica: p.ulica,
-      brojUlice: p.brojUlice,
-      postanskiBroj: p.postanskiBroj,
-      mestoPrebivalista: p.mestoPrebivalista,
-      drzavaPrebivalista: p.drzavaPrebivalista,
-      drzavljanstvo: p.drzavljanstvo,
-      podnosilacJeIPronalazac: p.podnosilacJeIPronalazac
-    }
-  }
-
-  setPodnosiocFormFromSelection(){
-    let currPodnosioc = this.podnosilac;
-    this.podnosiocZahteva.patchValue({
-      ime: currPodnosioc?.form.ime,
-      telefon: currPodnosioc?.form.telefon,
-      eMail: currPodnosioc?.form.eMail,
-      faks: currPodnosioc?.form.faks,
-      ulica: currPodnosioc?.form.ulica,
-      brojUlice: currPodnosioc?.form.brojUlice,
-      postanskiBroj: currPodnosioc?.form.postanskiBroj,
-      mestoPrebivalista: currPodnosioc?.form.mestoPrebivalista,
-      drzavaPrebivalista: currPodnosioc?.form.drzavaPrebivalista,
-      drzavljanstvo: currPodnosioc?.form.drzavljanstvo,
-      podnosilacJeIPronalazac: currPodnosioc?.form.podnosilacJeIPronalazac
-    });
-  }
-
-  resetPodnosiocForm(){
-    this.podnosiocZahteva.setValue({
-      ime: '',
-      telefon: '',
-      eMail: '',
-      faks: '',
-      ulica: '',
-      brojUlice: '',
-      postanskiBroj: '',
-      mestoPrebivalista: '',
-      drzavaPrebivalista: '',
-      drzavljanstvo: '',
-      podnosilacJeIPronalazac:false
-    });
-  }
-
   onSubmitBtnClick(): void {
 
-
     console.log(this.items);
-    console.log(this.ostaloRanije);
+    console.log(this.punomocnik);
     alert("usao")
 
+    console.log(this._getDocumentAsXML())
 
     // this.requestMaker
     //   .sendPatentRequest(this._getDocumentAsXML())
@@ -257,144 +178,208 @@ export class NewPatentFormComponent implements OnInit {
     // });
   }
 
-  addPodnosioc(): void {
-    this.saveChanges();
-    this.podnosilac.selected = false;
-    
-    this.resetPodnosiocForm();
-  }
-
-  toggleSelect(index: number): void {
-    this.saveChanges();
-    this.setPodnosiocFormFromSelection();
-  }
-
-  saveChanges() {
-    this.podnosilac.form = this.getPodnosiocFromForm();
-  }
 
   theSamePerson(): void {
     //todo podnosilac je ista osoba kao i pronalazac
+      this.podnosilacJeIPronalazac=!this.podnosilacJeIPronalazac
+  }
+
+  theSamePersonYetAgain(): void {
+    //todo pronalazac je ista osoba kao i podnosilac
+    this.pronalazacJeIPodnosilac=!this.pronalazacJeIPodnosilac
   }
 
   doesntWantToBeApointed(): void {
     //todo pronalazac ne zeli da bude naveden u prijavi
+    this.pronalazacNeZeliDaBudeNaveden=!this.pronalazacNeZeliDaBudeNaveden
   }
 
   forReception(): void {
     //todo ПУНОМОЋНИК ЗА ПРИЈЕМ ПИСМЕНА
+    this.zaPrijemPismena=!this.zaPrijemPismena
   }
 
   forRepresentation(): void {
     //todo  ПУНОМОЋНИК ЗА ЗАСТУПАЊЕ
+    this.zaZastupanje=!this.zaZastupanje
   }
 
   viaPapir(): void {
     //todo  Подносилац пријаве је сагласан да Завод врши достављање писмена у папирној форми 
+    this.papirnaForma=!this.papirnaForma
   }
 
   viaMail(): void {
     //todo  Подносилац пријаве је сагласан да Завод врши достављање писмена искључиво
     // електронским путем у форми електронског документа (у овом случају неопходна је 
     //регистрација на порталу "еУправе")
+    this.elektronskimPutem=!this.elektronskimPutem
   }
 
   separateEntry(): void {
     //todo  ИЗДВОЈЕНА ПРИЈАВА
+    this.izdvojena=!this.izdvojena
   }
 
   additionalEntry(): void {
     //todo  ДОПУНСКА ПРИЈАВА 
-  }
-
-  onCheckboxChange(e: any): void {
-    if (e.checked)
-      this.brojeviKlasaRobeIliUsluga.push(e.source.value);
-    else {
-      let i = 0;
-      this.brojeviKlasaRobeIliUsluga.forEach((item: any) => {
-        if (item == e.source.value) {
-          this.brojeviKlasaRobeIliUsluga.splice(i, 1);
-          return;
-        }
-        i++;
-      });
-    }
+    this.dopunska=!this.dopunska
   }
 
   _getDocumentAsXML(): any {
-    let punomocnik = this.punomocnik.value;
-    let zig = this.podaciPatenta.value;
-    let ostaloVrsta = this.ostaloVrsta.value;
-    let ostaloRanije = this.ostaloRanije.value;
-
     const reqBody = {
       "_attributes": {
-        "xmlns:z": "http://www.ftn.uns.ac.rs/zahtev_za_zig",
-        "xmlns:pred": "http://www.ftn.uns.ac.rs/zahtev_za_zig/pred/",
+        "xmlns:p": "http://www.ftn.uns.ac.rs/zahtev_za_patent",
+        "xmlns:pred": "http://www.ftn.uns.ac.rs/zahtev_za_patent/pred",
         "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
-        "xsi:schemaLocation": "http://www.ftn.uns.ac.rs/zahtev_za_zig ZahtevZaPriznjanjeZiga.xsd"
+        "xsi:schemaLocation": "http://www.ftn.uns.ac.rs/zahtev_za_patent Zahtev_za_patent.xsd"
       },
-      "z:MetaData": { 
-        "z:BrojPrijave": { _text: ""},
-        "z:EmailNalogaPodnosioca": { _text: this.lStoargeManager.getLoggedUserEmail() },
-        "z:DatumPodnosenja": { _text: new Date().toISOString().slice(0, 19) }, 
-        "z:DatumPregledanja": { _text: new Date().toISOString().slice(0, 19) }, 
-        "z:Status": { _text: ""},
+      "p:popunjava_zavod": { 
+        "p:broj_prijave": { _text: ""},
+        "p:datum_prijema": { _text: new Date().toISOString().slice(0, 19) },
+        "p:priznati_datum_podnosenja": { _text: new Date().toISOString().slice(0, 19) }, 
       }, 
-      "z:PodnosilacPrijave": this._getPersonInfoJSONObj(this.podnosilac) ,
-      "z:Punomocnik": this._getPersonInfoJSONObj(punomocnik),
-      "z:ZajednickiPredstavnik": this._getPersonInfoJSONObj(this.podnosilac.form),
-      // "z:Zig": this._getTrademarkInfoJSONObj(zig),
-      "z:BrojeviKlasaRobeIliUsluga": { "z:BrojKlaseRobeIliUsluge": this.brojeviKlasaRobeIliUsluga.sort((a,b) => a - b) },
-      "z:BrojPrvobitnePrijave": { _text: this.ostaloRanije.get("brojPrvobitnePrijave")  },
-      "z:DatumPodnosenjaPrvobitnePrijave": { _text: this.ostaloRanije.get("datumPodnosenjaPrvobitnePrijave") },
-      "z:DvoslovnaOznakaDrzave": { _text: this.ostaloRanije.get("dvoslovnaOznakaDrzave")},
-      // "z:Taksa": this._getTaxesInfoJSONObj(ostalo)
+      "p:naziv_pronalaska": this._getNazivInfoJSONObj(this.podaciPatenta.value) ,
+      "p:podnosilac_prijave": this._getPodnosilacInfoJSONObj(this.podnosiocZahteva.value) ,
+      "p:pronalazac": this._getPronalazacInfoJSONObj(this.pronalazac.value) ,
+      "p:punomocnik": this._getPunomocnikInfoJSONObj(this.punomocnik.value),
+      "p:dostavljanje_podaci": this._getDostavljanjeInfoJSONObj(this.podaciPosiljke.value),
+      "p:prijava": this._getOstaloVrstaPrijaveInfoJSONObj(this.ostaloVrsta.value),
+      "p:zahtev_za_priznanje_prava_prvenstva_iz_ranijih_prijava":{
+        "p:ranija_prijava": this._getRanijePrijaveInfoJSONObj(this.items),
+      } 
 
     };
-    return this.xmlParser.parseToXml("z:ZahtevZaPriznanjeZiga", reqBody);
+    return this.xmlParser.parseToXml("p:ZahtevZaPriznanjeZiga", reqBody);
+  }
+  _getRanijePrijaveInfoJSONObj(listaRanijihPrijava: any) {
+    let result: { "p:datum_podnosenja_ranije_prijave": { _text: any; }; "p:broj_ranije_prijave": { _text: any; }; "p:dvoslovna_oznaka_drzave": { _text: any; }; }[] = [];
+
+    listaRanijihPrijava.forEach((formData: { datumPodnosenjaPrvobitnePrijave: any; brojPrvobitnePrijave: any; dvoslovnaOznakaDrzave: any; }) => {
+      result.push({
+          "p:datum_podnosenja_ranije_prijave": { _text: formData.datumPodnosenjaPrvobitnePrijave },
+          "p:broj_ranije_prijave": { _text: formData.brojPrvobitnePrijave },
+          "p:dvoslovna_oznaka_drzave": { _text: formData.dvoslovnaOznakaDrzave }
+      });
+    });
+
+    return result;
   }
 
-  _getPersonInfoJSONObj(formData: any) {
+  _getOstaloVrstaPrijaveInfoJSONObj(formData: any) {
     return {
-      "z:Ime": { _text: formData.ime },
-      "z:Adresa": { 
-        "z:Ulica": { _text: formData.ulica},
-        "z:BrojUlice": { _text: formData.brojUlice},
-        "z:PostanskiBroj": { _text: formData.postanskiBroj},
-        "z:MestoPrebivalista": { _text: formData.mestoPrebivalista},
-        "z:DrzavaPrebivalista": { _text: formData.drzavaPrebivalista},
-        "z:Drzavljanstvo": { _text: formData.drzavljanstvo}
-      },
-      "z:KontaktInformacije": {
-        "z:Telefon": { _text: formData.telefon},
-        "z:E-mail": { _text: formData.eMail},
-        "z:Faks": { _text: formData.faks}
+      "p:dopunska_prijava": { _text: formData.dopunska },
+      "p:izdvojena_prijava": { _text: formData.izdvojena },
+      "p:broj_osnovne_prijave": { _text: formData.brojPrvobitnePrijave },
+      "p:datum_podnosenja_osnovne_prijave": { _text: formData.datumPodnosenjaPrvobitnePrijave },
+    }
+  }
+
+  _getNazivInfoJSONObj(formData: any) {
+    return {
+      "p:na_srpskom_jeziku": { _text: formData.naSrpskom },
+      "p:na_engleskom_jeziku": { _text: formData.naEngleskom },
+    }
+  }
+
+  _getDostavljanjeInfoJSONObj(formData: any) {
+    return {
+        "p:adresa": { 
+          "p:ulica": { _text: formData.ulicaDostavljanje  },
+          "p:broj_objekta": { _text: formData.brojUliceDostavljanje  },
+          "p:postanski_broj": { _text: formData.postanskiBrojDostavljanje },
+          "p:mesto": { _text: formData.mestoPrebivalistaDostavljanje },
+          "p:drzava": { _text: formData.drzavaPrebivalistaDostavljanje }
+        },
+        "p:podnosilac_saglasan_dostavljanje_elektronskim_putem": { _text: formData.elektronskimPutem},
+        "p:podnosilac_saglasan_dostavljanje_u_papirnoj_formi": { _text: formData.papirnaForma},
+      }
+    }
+
+
+  _getPodnosilacInfoJSONObj(formData: any) {
+    return {
+      // traba staviti vrednost checkboxa!!!
+      "p:podnosilac_prijave_je_i_pronalazac": { _text: this.podnosilacJeIPronalazac },
+      "p:licni_podaci": { 
+        "p:fizicko_pravno_lice": { 
+          "p:fizicko_lice": { 
+            "p:ime": { _text: formData.ime},
+            "p:prezime": { _text: formData.prezime},
+          }
+        },
+        "p:drzavljanstvo": { _text: formData.drzavljanstvo },
+        "p:adresa": { 
+          "p:ulica": { _text: formData.ulica },
+          "p:broj_objekta": { _text: formData.brojUlice },
+          "p:postanski_broj": { _text: formData.postanskiBroj },
+          "p:mesto": { _text: formData.mestoPrebivalista },
+          "p:drzava": { _text: formData.drzavaPrebivalista }
+        },
+        "p:dodatni_podaci": { 
+          "p:broj_telefona": { _text: formData.brojUlice},
+          "p:broj_faksa": { _text: formData.postanskiBroj},
+          "p:e_posta": { _text: formData.mestoPrebivalista},
+        }
       }
     }
   }
 
-  _getTrademarkInfoJSONObj(formData: any) {
-    // console.log(formData);
+  _getPronalazacInfoJSONObj(formData: any) {
     return {
-      // "z:UlicaDostavljanje": { _text: formData.tipZiga },
-      // "z:BrojUliceDostavljanje": { _text: formData.tipZnaka },
-      // "z:PostanskiBrojDostavljanje": { _text: formData.transliteracija },
-      // "z:MestoPrebivalistaDostavljanje": { _text: formData.prevodZnaka },
-      "z:NaEngleskom": { _text: formData.naEngleskom },
-      "z:NaSrpskom": { _text: formData.naSrpskom }
+      // traba staviti vrednost checkboxa!!!
+      "p:pronalazac_ne_zeli_da_bude_naveden_u_prijavi": { _text: formData.pronalazacNeZeliDaBudeNaveden  },
+      "p:pronalazac_je_podnosilac_prijave": { _text: formData.pronalazacJeIPodnosilac  },
+      "p:podaci_o_pronalazacu": { 
+        "p:fizicko_lice": { 
+          "p:ime": { _text: formData.ime},
+          "p:prezime": { _text: formData.prezime},
+        },
+        "p:adresa": { 
+          "p:ulica": { _text: formData.ulica },
+          "p:broj_objekta": { _text: formData.brojUlice },
+          "p:postanski_broj": { _text: formData.postanskiBroj },
+          "p:mesto": { _text: formData.mestoPrebivalista },
+          "p:drzava": { _text: formData.drzavaPrebivalista }
+        },
+        "p:dodatni_podaci": { 
+          "p:broj_telefona": { _text: formData.brojUlice},
+          "p:broj_faksa": { _text: formData.postanskiBroj},
+          "p:e_posta": { _text: formData.mestoPrebivalista},
+        }
+      }
     }
   }
 
-  _getTaxesInfoJSONObj(formData: any) {
+  _getPunomocnikInfoJSONObj(formData: any) {
     return {
-      "z:OsnovnaTaksa": { _text: formData.osnovnaTaksa },
-      "z:TaksaZaBrojKlasa": { _text: formData.taksaZaBrojKlasa },
-      "z:TaksaZaGrafickoResenje": { _text: formData.taksaZaGrafickoResenje },
-      "z:Ukupno": { _text: Number(formData.osnovnaTaksa) + Number(formData.taksaZaBrojKlasa) + Number(formData.taksaZaGrafickoResenje) }
+      // traba staviti vrednost checkboxa!!!
+      "p:vrsta_punomocja": { 
+        "p:punomocnik_za_zastupanje": { _text: formData.zaZastupanje  },
+        "p:punomocnik_za_prijem_pismena": { _text: formData.zaPrijemPismena  }
+      },
+     
+      "p:licni_podaci": { 
+        "p:fizicko_pravno_lice": { 
+          "p:fizicko_lice": { 
+            "p:ime": { _text: formData.ime},
+            "p:prezime": { _text: formData.prezime},
+          }
+        },
+        "p:drzavljanstvo": { _text: formData.drzavljanstvo },
+        "p:adresa": { 
+          "p:ulica": { _text: formData.ulica },
+          "p:broj_objekta": { _text: formData.brojUlice },
+          "p:postanski_broj": { _text: formData.postanskiBroj },
+          "p:mesto": { _text: formData.mestoPrebivalista },
+          "p:drzava": { _text: formData.drzavaPrebivalista }
+        },
+        "p:dodatni_podaci": { 
+          "p:broj_telefona": { _text: formData.brojUlice},
+          "p:broj_faksa": { _text: formData.postanskiBroj},
+          "p:e_posta": { _text: formData.mestoPrebivalista},
+        }
+      }
     }
   }
-
 }
-
