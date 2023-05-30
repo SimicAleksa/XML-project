bd12c3913a986eced49a4173230a0b3288db77b4
SimicAleksa <103002749+SimicAleksa@users.noreply.github.com>
2023-05-30 17:40:01 +0200

    xonomy dodatak

--- XML_WS_frontend/src/app/components/patent/xonomy/xonomy.service.ts
+++ XML_WS_frontend/src/app/components/patent/xonomy/xonomy.service.ts
@@ -22,9 +22,89 @@ export class XonomyService {
                     hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
                         return jsElement.hasChildElement("naziv_pronalaska")
                     },
-                }
+                },
+                {
+                  caption:'Dodaj <podnosilac_prijave>',
+                  action: Xonomy.newElementChild,
+                  actionParameter: '<podnosilac_prijave></podnosilac_prijave>',
+                  hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                      return jsElement.hasChildElement("podnosilac_prijave")
+                  },
+              }
                 ]
             },
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            /////////////////////////////////////////// podnosilac_prijave ////////////////////////////////////////////
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////
+            podnosilac_prijave : {
+              asker: Xonomy.askString,
+              menu:[
+              {
+                  caption:'Dodaj <podnosilac_prijave_je_i_pronalazac>',
+                  action: Xonomy.newElementChild,
+                  actionParameter: '<podnosilac_prijave_je_i_pronalazac></podnosilac_prijave_je_i_pronalazac>',
+                  hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                      return jsElement.hasChildElement("podnosilac_prijave_je_i_pronalazac")
+                  },
+              },
+              {
+                  caption:'Dodaj <licni_podaci>',
+                  action: Xonomy.newElementChild,
+                  actionParameter: '<licni_podaci></licni_podaci>',
+                  hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                      return jsElement.hasChildElement("licni_podaci")
+                  },
+              },
+              {
+                  caption: 'Obrisi naziv pronalaska',
+                  action: Xonomy.deleteElement
+              }]
+          },
+
+          licni_podaci : {
+            asker: Xonomy.askString,
+            menu:[
+            {
+                caption:'Dodaj <fizicko_pravno_lice>',
+                action: Xonomy.newElementChild,
+                actionParameter: '<fizicko_pravno_lice></fizicko_pravno_lice>',
+                hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                    return jsElement.hasChildElement("fizicko_pravno_lice")
+                },
+            },
+            {
+                caption:'Dodaj <drzavljanstvo>',
+                action: Xonomy.newElementChild,
+                actionParameter: '<drzavljanstvo></drzavljanstvo>',
+                hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                    return jsElement.hasChildElement("drzavljanstvo")
+                },
+            },
+            {
+              caption:'Dodaj <adresa>',
+              action: Xonomy.newElementChild,
+              actionParameter: '<adresa></adresa>',
+              hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                  return jsElement.hasChildElement("adresa")
+              },
+            },
+            {
+              caption:'Dodaj <dodatni_podaci>',
+              action: Xonomy.newElementChild,
+              actionParameter: '<dodatni_podaci></dodatni_podaci>',
+              hideIf: function (jsElement: { hasChildElement: (arg0: string) => any; }) {
+                  return jsElement.hasChildElement("dodatni_podaci")
+              },
+            },
+            {
+              caption:'Obrisi licne podatke',
+              action:Xonomy.deleteElement
+            }
+            ]
+        },
+
+
             
             
 
@@ -56,7 +136,6 @@ export class XonomyService {
                 }]
             },
             na_srpskom_jeziku : {
-                mustBeBefore: ['na_engleskom_jeziku'],
                 hasText: true,
                 oneliner: true,
                 asker: Xonomy.askString,
